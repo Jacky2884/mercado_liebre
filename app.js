a17e345919b85877b6eef0561a5ac5ef98292e5c
Jacky2884 <jacky_morales84@hotmail.com>
2023-11-05 20:40:10 -0300

    alcualizando datos

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'views', 'main.html'));
+});
+
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src','views', 'register.html'));
+});
+
+app.get('/iniciar-sesion', (req, res) => {
+    res.sendFile(path.join(__dirname,'src', 'views','iniciar-sesion.html'));
+});
+
+const publicPath = path.join(__dirname,'src', 'public');
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Servidor Express escuchando en http://localhost:${port}`);
+});
